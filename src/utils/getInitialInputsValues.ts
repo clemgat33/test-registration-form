5a5a9408330f136dddf405faa27062bd21bd75d8
cidgi359 <74004967+cidgi359@users.noreply.github.com>
2021-07-25 20:12:16 +0300

    remove select first option

--- src/utils/getInitialInputsValues.ts
+++ src/utils/getInitialInputsValues.ts
@@ -8,12 +8,7 @@ export function getInitialInputsValues(): Inputs {
 	controller.map((step) => {
 		step.fields.map((field: Field) => {
 			const name = field.label.replace(/\s/g, '').toLowerCase();
-			//add the first option in the select component, because it will be empty if the client doesnt choose any
-			if(field.type === 'select' && field.options){
-				initialValues = {...initialValues, [name]: field.options[0]};
-			} else {
-				initialValues = {...initialValues, [name]: ''};
-			}
+			initialValues = {...initialValues, [name]: ''};
 		});
 	});
 	return initialValues;
